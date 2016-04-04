c70b4573a3b6f854c4cb8ab98ac9a5f9aba54ff8
Jacob Perron <jacobmperron@gmail.com>
2016-04-04 12:16:17 -0700

    Add private node handle for paramters Made pose topic generic, remap in vicon specific launch file Remove temporary files, add .gitignore Minor refactoring of source files Add config file

--- include/recharge_behaviour/recharge_behaviour.h
+++ include/recharge_behaviour/recharge_behaviour.h
@@ -4,25 +4,25 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Twist.h>
-//#include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <std_msgs/Empty.h>
 #include <std_msgs/Float32.h>
+
 #define PI 3.14159
 #define TWO_PI 6.283185
 
 class RechargeBehaviour {
-  private:
+private:
 	/*Controls the loop rate*/
-    	double loopHz;
+ 	double loopHz;
 	/*The robot's yaw, extracted from the localization subscription*/
-    	double yaw;
+ 	double yaw;
 	/*These two check that we're receiving poses and charge readings from the two subscriptions, the loop doesn't work without them*/
-    	bool poseReceived;
+ 	bool poseReceived;
 	bool chargeReceived;
 	/*Used while homing in on recharge location, to differentiate between when you stop to turn toward the target and start driving forward*/
 	bool driving;
-	
+
 	/*parameters that control when the robot goes to recharge if using battery level information*/
 	double chargeLevel;
 	double highThreshold;
@@ -33,11 +33,10 @@ class RechargeBehaviour {
 	bool  recharging;
 	int chargeState;
 
-	/*parameters that name the robot, give it the vicon topic to listen to for its localization and specify where its charger is in the world*/
+	/*parameters that name the robot, and specify where its charger is in the world*/
 	double chargerX;
 	double chargerY;
 	std::string robotName;
-	std::string viconTopic;
 
 	/*parameters that control when the robot goes to recharge if using time*/
 	ros::Time cycleStartTime;
@@ -49,35 +48,35 @@ class RechargeBehaviour {
 	double midChargeTime;
 	double lowChargeTime;
 
-    //void targetPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose);
-    	void ownPoseCallback(const geometry_msgs::TransformStamped::ConstPtr& pose);
+  void ownPoseCallback(const geometry_msgs::TransformStamped::ConstPtr& pose);
 	void chargeLevelCallback(const std_msgs::Float32 charge);
 	float getDesiredAngle(float targetX, float targetY, float currentXCoordinateIn, float currentYCoordinateIn);
 	void approachCharger();
 	void whileActive();
 	void whileRecharging();
 
-  protected:
-    	ros::NodeHandle nh;
-    	ros::Subscriber ownPoseSub;
+protected:
+  ros::NodeHandle nh;
+  ros::NodeHandle privNh;
+ 	ros::Subscriber ownPoseSub;
 	ros::Subscriber chargeLevelSub;
 
 	ros::Publisher cmd_vel_pub;
 	ros::Publisher dock_pub;
 	ros::Publisher undock_pub;
 
-    	geometry_msgs::TransformStamped ownPose;
+ 	geometry_msgs::TransformStamped ownPose;
 
 	/*Movement orders for Create*/
 	geometry_msgs::Twist move_cmd;
 
 
-  public:
-    RechargeBehaviour(ros::NodeHandle& nh);
-    ~RechargeBehaviour();
+public:
+  RechargeBehaviour(ros::NodeHandle& nh);
+  ~RechargeBehaviour();
 
-    virtual void spin();
-    virtual void spinOnce();
+  virtual void spin();
+  virtual void spinOnce();
 
 }; // class RechargeBehaviour
 
